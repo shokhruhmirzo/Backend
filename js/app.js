7db222df3aba140f845c3900280bf651a1ac32bd
shokhruhmirzo <shaalimov923@gmail.com>
2024-03-09 07:44:03 +0500

    create website

---
+++ js/app.js
@@ -0,0 +1,49 @@
+// fetch('https://jsonplaceholder.typicode.com/users')
+
+// .then((res)=> res.json())
+// .then((data)=> console.log(data))
+
+function setBooks(e) {
+    e.preventDefault()
+    console.log(e.target.title.value);
+    console.log(e.target.img.value);
+    console.log(e.target.price.value);
+
+    let books = {
+        title: e.target.title.value,
+        img: e.target.img.value,
+        price: e.target.price.value
+    }
+    fetch('https://65eb326743ce1641893373f6.mockapi.io/OtkanKunlar', {
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify(books)
+    })
+        .then((res) => res.json())
+        .then((data) => console.log(data))
+}
+let elListcha = document.querySelector('.list')
+
+fetch('https://65eb326743ce1641893373f6.mockapi.io/OtkanKunlar')
+    .then((res) => res.json())
+    .then((data) => {
+        data.forEach(element => {
+            let newLi = document.createElement('li')
+            newLi.innerHTML = `
+            <img src=${element.img} width="100"  alt="">
+             <p>${element.price}</p>
+            <button onclick="removeInf(${element.id})">${element.title}</button>`
+            elListcha.appendChild(newLi)
+        });
+    })
+
+function removeInf(id) {
+    console.log(id);
+    fetch(`https://65eb326743ce1641893373f6.mockapi.io/OtkanKunlar/${id}`, {
+        method: 'DELETE',
+        headers: { 'content-type': 'application/json' },
+    })
+        .then((res) => res.json())
+        .then((data) => console.log(data))
+}
+
